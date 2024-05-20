98d704bf1c9e7852a61d853e5ca2973fd8b15896
Fabrice243 <152931886+Fabrice243@users.noreply.github.com>
2024-05-20 10:40:30 -0400

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,1 +1,27 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div>&copy; Space-Explorer</div>
+            <div className={styles.infoSection}>
+                <div className={styles.column}>
+                    <h4>Information</h4>
+                    <ul>
+                        <li><a href="/a-propos-de-nous">À Propos De Nous</a></li>
+                        <li><a href="/contactez-nous">Contactez-Nous</a></li>
+                        <li><a href="/politique-de-confidentialite">Politique De Confidentialité</a></li>
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    <h4>Besoin D'aide?</h4>
+                    <ul>
+                        <li><a href="/votre-compte">Votre Compte</a></li>
+                        <li><a href="/faq">FAQ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+}
 
