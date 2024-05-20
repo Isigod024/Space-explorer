c8d446290e1712541664347447d28c13ad60464d
Isigod024 <150750800+Isigod024@users.noreply.github.com>
2024-05-19 22:49:58 -0400

    SuperUpdateReset

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,17 @@
+import Header from '@/components/Header';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Test from '@/components/Header';
-
 const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
-  title: 'Space Explorer',
-  description: '',
+  title: 'Biblio-app',
+  description: 'Demo Cours Programmation web avancé',
 };
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <Header />
+        <Header />
         {children}
       </body>
     </html>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,13 @@
 import styles from './Header.module.css'
 import Image from 'next/image'
 import logo from '@/public/react.webp'
-import NavBar from './NavBar'
+import MenuNav from './MenuNav'
 import { IoMdMenu } from "react-icons/io";
 
 export default function Header() {
 return <header className={styles.header}>
-    <NavBar/>
+    <MenuNav />
+    <MenuNav />
+    <MenuNav />
     </header>
 }
