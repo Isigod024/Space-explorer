e84c2d30e32287f03cf4c83dcb70ecdc96c3dd57
BILLYMQ <njoheyoboyanik@gmail.com>
2024-05-21 11:32:04 -0400

    mm

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
 import styles from "./page.module.css";
+import image from 'next/image';
+import neb from '@/public/nebuleuse.webp'
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.welcome}>
         Soyez la bienvenue sur Space-Explorer
+      <img src="nebuleuse.webp" alt="image de nebuleuse" />
       </div>
     </main>
   );
