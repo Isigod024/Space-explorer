458b20c54f707a14d54f0e21331f3724033d536f
Isigod024 <150750800+Isigod024@users.noreply.github.com>
2024-05-23 14:32:46 -0400

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,19 @@
 import styles from './Header.module.css'
 import Image from 'next/image'
-import logo from '@/public/react.webp'
+import logo from '@/public/SpaceExplorerLogo.webp'
 import NavBar from './NavBar'
 import SearchBar from './Search'
 import { IoMdMenu } from "react-icons/io";
 
 export default function Header() {
 return <header className={styles.header}>
+    <Image 
+        src = {logo} 
+        alt = "logo de notre site web"
+        height={100}
+        width={100}
+    />
+    <h1>SPACE-EXPLORER</h1>
     <NavBar />
     <SearchBar />
     </header>
