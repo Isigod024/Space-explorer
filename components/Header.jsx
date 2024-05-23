c9d23283521ef9274a9e1a3610b2853169ef9219
Isigod024 <150750800+Isigod024@users.noreply.github.com>
2024-05-23 16:13:46 -0400

    logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,43 @@
-import styles from './Header.module.css'
-import Image from 'next/image'
-import logo from '@/public/SpaceExplorerLogo.webp'
-import NavBar from './NavBar'
-import SearchBar from './Search'
-import { IoMdMenu } from "react-icons/io";
+"use client";
+import React, { useState, useEffect } from 'react';
+import styles from './Header.module.css';
+import Image from 'next/image';
+import logo from '@/public/SpaceExplorerLogo.webp';
+import NavBar from './NavBar';
+import SearchBar from './Search';
 
 export default function Header() {
-return <header className={styles.header}>
-    <Image 
-        src = {logo} 
-        alt = "logo de notre site web"
-        height={100}
-        width={100}
-    />
-    <h1>SPACE-EXPLORER</h1>
-    <NavBar />
-    <SearchBar />
-    </header>
+    const [showHeader, setShowHeader] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > lastScrollY && window.scrollY > 100) {
+                setShowHeader(false);
+            } else {
+                setShowHeader(true);
+            }
+            setLastScrollY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [lastScrollY]);
+
+    return (
+        <header className={`${styles.header} ${showHeader ? '' : styles.hide}`}>
+            <Image 
+                src={logo} 
+                alt="logo de notre site web"
+                height={100}
+                width={100}
+            />
+            <h1>SPACE-EXPLORER</h1>
+            <NavBar />
+            <SearchBar />
+        </header>
+    );
 }
