7e91d6191848f9331eec2318d6c0c02ea3863ad6
BILLYMQ <njoheyoboyanik@gmail.com>
2024-05-25 23:03:22 -0400

    h

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,40 @@
+'use client'
 import Header from '@/components/Header';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Footer from '@/components/Footer';
-
+import Documents from '@/components/Documents';
+import Contact from '@/components/Contact';
+import Connexion from '@/components/Connexion';
+import Accueil from '@/components/Accueil';
+import { useState } from 'react';
+import styles from './layout.module.css';
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
-  title: 'Space-Explorer',
-  description: '',
-};
 export default function RootLayout({ children }) {
-  return (
+  const [page, setPage] = useState('accueil');
+return (
     <html lang='en'>
       <body className={inter.className}>
-        <Header />
+        <Header setPage={setPage} />
         {children}
-        <Footer/>
+        <main className={styles.main}>
+          {page === 'accueil' ? (
+            <Accueil />
+          ) : page === 'Galerie' ? (
+            <Galerie />
+          ) : page === 'Article' ? (
+            <article />
+          ) : page === 'Ajout d article' ? (
+            <AjoutDArticle />
+          ) : page === 'connexion' ? (
+            <Connexion />
+          ) : page === 'Contact' ? (
+            <Contact />
+          ) : (
+            <div>404 - Not Found</div>
+          )}
+        </main>
+        <Footer />
       </body>
     </html>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,12 +2,14 @@ import styles from './Header.module.css'
 import Image from 'next/image'
 import logo from '@/public/react.webp'
 import NavBar from './NavBar'
+import MenuNav from './MenuNav';
+
 import SearchBar from './Search'
 import { IoMdMenu } from "react-icons/io";
 
-export default function Header() {
+export default function Header({setPage}) {
 return <header className={styles.header}>
-    <NavBar />
+    <MenuNav setPage={setPage} />
     <SearchBar />
     </header>
 }
