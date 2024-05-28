ce820631cfb9e430b5ca79d7aa2df02ee049fd6d
Isigod024 <150750800+Isigod024@users.noreply.github.com>
2024-05-28 10:47:19 -0400

    Update Accueil.jsx

---
+++ components/Accueil.jsx
@@ -0,0 +1,12 @@
+import styles from "./Accueil.module.css";
+import image from 'next/image';
+import neb from '@/public/nebuleuse.webp'
+import Banniere from "@/components/Banniere";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  );
+}
