26615bb5f7d94a85d9b9c1ca4d972716e8786c4c
BILLYMQ <njoheyoboyanik@gmail.com>
2024-05-28 11:28:05 -0400

    ban

---
+++ components/Banniere.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './Banniere.Module.css';
+
+export default function Banniere({ image, title, description, height }) {
+  return (
+    <div className={styles.banniere} style={{ backgroundImage: {image} }}>
+      <div className={styles.content}>
+        
+        <h1 className={styles.title}>{title}
+        <Image src = {image} height={height}/>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </div>
+  );
+}
