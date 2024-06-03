3560c1bee147cc968ae0b3ee0a9695aaa00d69f9
Isigod024 <150750800+Isigod024@users.noreply.github.com>
2024-06-03 15:06:12 -0400

    Update Accueil.jsx

--- components/Accueil.jsx
+++ components/Accueil.jsx
@@ -1,26 +1,32 @@
-import styles from './Accueil.module.css';
-import Citation from './Citation';
-import Banniere from './Banniere';
-import nebuleuse from '@/public/nebuleuse.webp'
+import galaxyGif from '@/public/Galaxy.gif';
+import ArticleNode from './ArticleNode';
+import articles from '@/data/articles.json';
+import Image from 'next/image';
 
-import Timer from './Timer';
 export default function Accueil() {
-    return <>
-        <Citation auteur=''>
-            
-        </Citation>
-
-        <Banniere
-        image={nebuleuse}
-        title="Nebuleuse"
-        description="Description"
-        />
-        
-        <Timer/>
-        <Timer compte={25}/>
-        <Timer compte={10}/>
-        <div className={styles.welcome}>
-            
+    return (
+        <div className={styles.container}>
+            <div className={styles.imageContainer}>
+                <Image src={galaxyGif} alt="Galaxy" layout="fill" objectFit="cover" />
+            </div>
+            <h1 className={styles.mainTitle}>Bienvenue sur SPACE EXPLORER</h1>
+            <p className={styles.mainText}>Découvrez de nombreux articles, faits et images en rapport avec l'actualité spatiale</p>
+            <div className={styles.articleSection}>
+                <h2 className={styles.articleTitle}>Articles</h2>
+                <div className={styles.articleGrid}>
+                    {articles.map((article, index) => (
+                        <ArticleNode 
+                            key={index}
+                            id={index.toString()}
+                            title={article.title}
+                            image={article.image}
+                            content={article.content}
+                            date={article.date}
+                            author={article.author}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
-    </>
+    );
 }
