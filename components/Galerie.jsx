d28c03ef13a126a05f6ba9d3ed31ba1b354811c9
BILLYMQ <njoheyoboyanik@gmail.com>
2024-06-09 01:34:32 -0400

    h

--- components/Galerie.jsx
+++ components/Galerie.jsx
@@ -1,45 +1,4 @@
-// import React, { useState } from 'react';
-// import styles from './Galerie.module.css';
 
-// const Galerie = () => {
-//   const [images, setImages] = useState([]);
-//   const [imageUrl, setImageUrl] = useState('');
-
-//   const handleAddImage = () => {
-//     if (imageUrl) {
-//       setImages([...images, { src: imageUrl }]);
-//       setImageUrl('');
-//     }
-//   };
-
-//   return (
-//     <div className={styles.galerieContainer}>
-//       <div className={styles.formContainer}>
-//         <input
-//           type="text"
-//           value={imageUrl}
-//           onChange={(e) => setImageUrl(e.target.value)}
-//           placeholder="Enter image URL"
-//         />
-//         <button onClick={handleAddImage}>Add Image</button>
-//       </div>
-//       <div className={styles.galerie}>
-//         {images.map((image, index) => (
-//           <img key={index} src={image.src} alt={`Image ${index}`} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Galerie;
-
-// gallery.js
-// Galerie.jsx
-// Galerie.jsx
-// Galerie.jsx
-// Galerie.jsx
-// Galerie.jsx
 'use client';
 import React, { useState, useEffect } from 'react';
 import articles from '../data/articles';
@@ -149,3 +108,5 @@ const Galerie = () => {
 };
 
 export default Galerie;
+
+
