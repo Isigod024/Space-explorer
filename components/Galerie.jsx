887fd38f6728df1c7a592cd89bfc3ab73b0c2f22
BILLYMQ <njoheyoboyanik@gmail.com>
2024-06-09 01:05:33 -0400

    i

--- components/Galerie.jsx
+++ components/Galerie.jsx
@@ -40,14 +40,27 @@
 // Galerie.jsx
 // Galerie.jsx
 // Galerie.jsx
-
-'use client'
-import React, { useState } from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import articles from '../data/articles';
 import styles from './Galerie.module.css';
 
 const Galerie = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [userImages, setUserImages] = useState([]);
+  const [newImage, setNewImage] = useState({
+    lien_image: '',
+    titre_image: '',
+    description_image: ''
+  });
+
+  useEffect(() => {
+    // Load user images from local storage
+    const storedImages = JSON.parse(localStorage.getItem('userImages'));
+    if (storedImages) {
+      setUserImages(storedImages);
+    }
+  }, []);
 
   const images = articles.articles.reduce((acc, article) => {
     acc.push(article.image_principale);
@@ -65,10 +78,30 @@ const Galerie = () => {
     setSelectedImage(null);
   };
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewImage(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const updatedImages = [...userImages, newImage];
+    setUserImages(updatedImages);
+    localStorage.setItem('userImages', JSON.stringify(updatedImages)); // Save to local storage
+    setNewImage({
+      lien_image: '',
+      titre_image: '',
+      description_image: ''
+    });
+  };
+
   return (
     <div>
       <div className={styles.gallery}>
-        {images.map((image, index) => (
+        {[...images, ...userImages].map((image, index) => (
           <div key={index} className={styles.card} onClick={() => handleImageClick(image)}>
             <img src={image.lien_image} alt={image.titre_image} />
           </div>
@@ -85,6 +118,32 @@ const Galerie = () => {
           </div>
         </div>
       )}
+      <form className={styles.form} onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          name="lien_image"
+          value={newImage.lien_image}
+          onChange={handleInputChange}
+          placeholder="Image URL"
+          required
+        />
+        <input
+          type="text"
+          name="titre_image"
+          value={newImage.titre_image}
+          onChange={handleInputChange}
+          placeholder="Title"
+          required
+        />
+        <textarea
+          name="description_image"
+          value={newImage.description_image}
+          onChange={handleInputChange}
+          placeholder="Description"
+          required
+        />
+        <button type="submit">Add Image</button>
+      </form>
     </div>
   );
 };
