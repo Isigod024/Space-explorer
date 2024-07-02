59d511ac6e66a7833868c53c356e09eed6a7cc80
BILLYMQ <njoheyoboyanik@gmail.com>
2024-07-02 18:30:13 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,33 +1,27 @@
-'use client';
+// app/layout.jsx
 import Header from '@/components/Header';
 import { Inter } from "next/font/google";
 import Footer from '@/components/Footer';
 import ToggleButton from '@/components/ToggleButton';
 import "./globals.css";
 import styles from './layout.module.css';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n'; // Assurez-vous que le chemin est correct
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: 'biblio-app | Accueil',
-  description: 'Site web présentant les fonctionnalités de React et Next.js.',
-  openGraph: {
-    title: 'biblio-app | Accueil',
-    description: 'Site web présentant les fonctionnalités de React et Next.js.',
-    images: ['./react.webp']
-  }
-}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} ${styles.body}`}>
-        <Header />
-        <ToggleButton />
-        <main className={styles.main}>
-          {children}
-        </main>
-        <Footer />
+        <I18nextProvider i18n={i18n}>
+          <Header />
+          <ToggleButton />
+          <main className={styles.main}>
+            {children}
+          </main>
+          <Footer />
+        </I18nextProvider>
       </body>
     </html>
   );
