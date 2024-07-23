7b052b1c9db85ddbb5983431f1345c3d55d1f178
BILLYMQ <njoheyoboyanik@gmail.com>
2024-07-23 01:18:04 -0400

    h

--- app/next-i18next.config.js
+++ app/next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'fr'],
-    },
-  };
-  
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'], // Ajoutez ici toutes les langues que vous souhaitez prendre en charge
+  },
+  localePath: path.resolve('./public/locales'), // Assurez-vous d'avoir un répertoire locales dans public
+};
